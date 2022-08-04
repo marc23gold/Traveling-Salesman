10c3f27ab04186aa8b6d74c7e4e968c81e8a6fa4
Marcx <haitianhero6@gmail.com>
2022-08-04 17:09:29 -0400

    hashtable work

--- hash.py
+++ hash.py
@@ -6,35 +6,34 @@ class Hashtable:
         #use empty list as a container for the buckets
         self.table = []
         #for loop that initalizes all the empty buckets
+        #inserting 40 empty arrays into an empty array so: [[][][]...[]]
         for i in range(bucket_size):
             self.table.append([])
 
     #Inserts a new item into the hash table.
     def insert(self, key, item):
         #hash function by division
-        bucket = hash(item) % len(self.table)
+        #bucket is being used as the index number for the table that contains
+        #the 40 empty arrays so the function is using the division method
+        bucket = int(key) % len(self.table)
         #index aka the bucket_list number is assigned from the bucket variable
         #which contains the hash function that deterens where the item will go
         bucket_list = self.table[bucket]
         #insert the item at the end of the bucket list
-        bucket_list.append(item)
-
-
-        for kv in bucket_list:
-            #prints the key value
-            if kv[0] == key:
-                kv[1] = item
-                return True
         key_value = [key, item]
-        bucket_list.append(key_value)
-        return True
+
+        if bucket_list is None:
+            bucket_list = self.table[bucket].append(key_value)
+            return True
+        else 
+        #bucket_list.append(item)
 
         # Searches for an item with matching key in the hash table.
         # Returns the item if found, or None if not found.
 
     def search(self, key):
         # get the bucket list where this key would be.
-        bucket = hash(key) % len(self.table)
+        bucket = int(key) % len(self.table)
         bucket_list = self.table[bucket]
 
         # search for the key in the bucket list
@@ -50,12 +49,25 @@ class Hashtable:
 
     def remove(self, key):
         # get the bucket list where this item will be removed from.
-        bucket = hash(key) % len(self.table)
+        bucket = int(key) % len(self.table)
         bucket_list = self.table[bucket]
 
         # remove the item from the bucket list if it is present.
         if key in bucket_list:
             bucket_list.remove(key)
+
+
+myHash = Hashtable()
+myHash.insert('1', "ove")
+print(myHash.table)
+
+myHash.insert('2', "asdgasg")
+print(myHash.table)
+
+print(myHash.search('Jane Doe'))
+
+myHash.remove('2')
+print(myHash.table)
 
 
 
