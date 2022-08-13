751b3ac86f7a8280cc4d6387e26d3355ae410298
Marcx <haitianhero6@gmail.com>
2022-08-13 07:58:01 -0400

    added a function call to the address.csv file stored in a variable UI file still runs algo still runs

--- algo.py
+++ algo.py
@@ -1,7 +1,19 @@
-# from distances import *
-# from package import *
-# from readcsv import *
-# from truck import *
+from distances import *
+from package import *
+from readcsv import *
+from truck import *
+
+addressList = loadAddresses('address.csv')
+#print(addressList)
+
+
+
+
+
+
+
+
+
 #
 # #vectorToAddressDictionary = {
 #     # 0: 'HUB',
