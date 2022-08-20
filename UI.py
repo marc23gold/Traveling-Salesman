5032720a59e4940eb7f7a5956a4b43f93e60b6e9
Marcx <haitianhero6@gmail.com>
2022-08-20 14:46:43 -0400

    ok

--- UI.py
+++ UI.py
@@ -5,25 +5,67 @@ import datetime
 from readcsv import *
 from truck import *
 
-user = input("Please put in the hour you want: ")
-userMinutes = input("Please put in the minute you want: ")
-userTime = datetime.timedelta(hours = int(user), minutes=int(userMinutes))
-for x in range(1,41):
-    allPacks = packHash.search(x)
-    if allPacks.deliveryTime == None:
-        pass
-    elif allPacks.deliveryTime < userTime:
-        allPacks.status = "Delivered"
-    elif  userTime > allPacks.departureTime:
-        allPacks.status = "In Route"
-    else:
-        allPacks.status = "At the Hub"
+def showAllPackagesWithGivenDeadlineTime():
+    user = input("Please put in the hour you want: ")
+    userMinutes = input("Please put in the minute you want: ")
+    userTime = datetime.timedelta(hours = int(user), minutes=int(userMinutes))
+    for x in range(1,41):
+        allPacks = packHash.search(x)
+        if allPacks.deliveryTime == None:
+            pass
+        elif allPacks.deliveryTime < userTime:
+            allPacks.status = "Delivered"
 
-    print(allPacks)
+        elif  userTime > allPacks.departureTime:
+            allPacks.status = "In Route"
+            allPacks.deliveryTime = None
+        else:
+            allPacks.status = "At the Hub"
+            allPacks.deliveryTime = None
 
-total = (truck1.mileage + truck2.mileage + truck3.mileage)
-print("\nThe total milage is: ",total)
+        print(allPacks)
+
+    total = (truck1.mileage + truck2.mileage + truck3.mileage)
+    print("\nThe total milage is: ",total)
 
 
+
+
+def startEnd():
+    startUser = input("Please put in the start hour you want: ")
+    startUserMinutes = input("Please put in the minute you want: ")
+    endUser = input("Please put in the end hour you want: ")
+    endUserMinutes = input("Please put in the minute that you want: ")
+
+    startUserTime = datetime.timedelta(hours = int(startUser), minutes=int(startUserMinutes))
+    endUserTime = datetime.timedelta(hours = int(endUser), minutes=int(endUserMinutes))
+    for x in range(1,41):
+        allPacks = packHash.search(x)
+        if allPacks.deliveryTime == None:
+            pass
+        elif allPacks.deliveryTime >= startUserTime and allPacks.deliveryTime <= endUserTime:
+            allPacks.status = "Delivered"
+
+        elif startUserTime > allPacks.departureTime:
+            pass
+        else:
+            allPacks.deliveryTime = ""
+            allPacks.ID = ""
+            allPacks.address = ""
+            allPacks.city = ""
+            allPacks.state = ""
+            allPacks.zip = ""
+            allPacks.deadline = ""
+            allPacks.mass = ""
+            allPacks.notes = ""
+            allPacks.status = ""
+
+
+        print(allPacks)
+
+    total = (truck1.mileage + truck2.mileage + truck3.mileage)
+    print("\nThe total milage is: ",total)
+
+startEnd()
 
 
--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-#Basic UI call function from command line
-#basic imports
+#Marcxime Prosper Student ID: 009463957
+from UI import *
 import datetime
 
-from readcsv import *
-from truck import *
 
-"""def UI(#whatever, whatever):
-        if #condition on whether to print one statement or everyithg:
-            #statements to print
-        else:
-            #print all state ments """
-user = input("Please put in the time you want in the format: ")
-userTime = datetime.timedelta(hours = int(user))
-for x in range(1,41):
-    allPacks =packHash.search(x)
-    if allPacks.deliveryTime == None:
-        pass
-    elif allPacks.deliveryTime < userTime:
-        allPacks.status = "Delivered"
-    elif  userTime > allPacks.departureTime:
-        allPacks.status = "In Route"
-    else:
-        allPacks.status = "At the Hub"
 
-    print(allPacks)
-
-total = (truck1.mileage + truck2.mileage + truck3.mileage)
-print("\nThe total milage is: ",total)
-print("\nRerun the program if you want to try again")
 
 
 
